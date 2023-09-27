264af174c566336d23578b4c1c819d18af6cb150
Felixalvar <114879520+Felixalvar@users.noreply.github.com>
2023-09-27 20:31:03 +0200

    Update identificadorAves.py

--- pythonProject/venv/AvesApp/identificadorAves.py
+++ pythonProject/venv/AvesApp/identificadorAves.py
@@ -38,16 +38,10 @@ nombreAve = tamanoAve = habitatAve = comportamientoAve = colorAve = patasColor =
 with st.sidebar:
     # Informamos de los pasos a seguir
     st.subheader('_Uso del buscador:_')
-    st.write(
-        """
-        **_Si sabes el ave, pincha en la casilla de verificación 'Ave' más abajo y búscala en 
-             el desplegable. 
-             Si no la sabes, sigue las instrucciones:_**
-             """
-    )
+    st.write("**_Si sabes el ave, pincha en la casilla de verificación 'Ave' más abajo y búscala en 
+             el desplegable. Si no la sabes, sigue las instrucciones:_**")
     # st.write("**_Si no la sabes, sigue las instrucciones:_**")
-    st.write(
-        "**_1. Para iniciar la identificación elige algún filtro, pincha en la flecha "
+    st.write("**_1. Para iniciar la identificación elige algún filtro, pincha en la flecha "
         "del cuadro de selección y busca una opción._**")
     st.write("**_2. Con cada filtro elegido vamos cribando el número de aves._**")
     st.write("**_3. Ve eligiendo el filtro que más te convenga._**")
