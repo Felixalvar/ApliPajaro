a4bebb0ca4bc344474342866a3be91aa66e3d972
Felixalvar <114879520+Felixalvar@users.noreply.github.com>
2023-09-27 20:14:53 +0200

    Update identificadorAves.py

--- pythonProject/venv/AvesApp/identificadorAves.py
+++ pythonProject/venv/AvesApp/identificadorAves.py
@@ -39,7 +39,7 @@ with st.sidebar:
     # Informamos de los pasos a seguir
     st.subheader('_Uso del buscador:_')
     st.write("**_Si sabes el ave, pincha en la casilla de verificación 'Ave' más abajo y búscala en "
-             "el desplegable. Si no la sabes, sigue las instrucciones:_**")
+             "el desplegable.            Si no la sabes, sigue las instrucciones:_**")
     # st.write("**_Si no la sabes, sigue las instrucciones:_**")
     st.write(
         "**_1. Para iniciar la identificación elige algún filtro, pincha en la flecha "
