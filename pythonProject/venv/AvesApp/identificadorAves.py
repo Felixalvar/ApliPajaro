7ee360e6fa7e27402bed9c3b0229613453c7e74b
Felixalvar <114879520+Felixalvar@users.noreply.github.com>
2024-05-26 20:39:13 +0200

    Update identificadorAves.py

--- pythonProject/venv/AvesApp/identificadorAves.py
+++ pythonProject/venv/AvesApp/identificadorAves.py
@@ -26,6 +26,7 @@ st.caption(
     **Espacio declarado Red Natura 2000 y** 
     **Zona de Especial Protección para las Aves (ZEPA)**.
     https://gl.wikipedia.org/wiki/Rede_Natura_2000  
+    
     **Comarca do Baixo Miño**.
     https://gl.wikipedia.org/wiki/Comarca_do_Baixo_Mi%C3%B1o
     
