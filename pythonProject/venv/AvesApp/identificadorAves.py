8a5b9c7bc79591a437885eb587c483716f019189
Felixalvar <114879520+Felixalvar@users.noreply.github.com>
2025-01-07 13:34:21 +0100

    Update identificadorAves.py

--- pythonProject/venv/AvesApp/identificadorAves.py
+++ pythonProject/venv/AvesApp/identificadorAves.py
@@ -34,6 +34,8 @@ st.caption(
     https://gl.wikipedia.org/wiki/Zona_especial_de_conservaci%C3%B3n_do_Baixo_Mi%C3%B1o
     """
 )
+st.write(
+            '**_______________________________________________________________________________________________________________________**')
 # Inicializamos todos los filtros de la barra lateral con una cadena vacía para que al iniciar la aplicación no dé
 # ningún error.
 nombreAve = tamanoAve = habitatAve = comportamientoAve = colorAve = patasColor = picoColor = picoForma = picoGrorsor = \
